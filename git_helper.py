356ec65dd51ef57df3cb120de19e770686b77251
Bram Adams <3282661+bramses@users.noreply.github.com>
2022-07-06 23:36:16 -0500

    git_helper

--- git_helper.py
+++ git_helper.py
@@ -3,3 +3,19 @@
 #    cons: a ton of http calls
 # option 2: keep the folder locally
 #    cons: cannot gitignore files
+# option 3: point to a remote folder
+#    point to a directory that is seperately managed by git
+#    fetch using git fetch to get filenames
+
+# do not reindex the entire index every time a file is added
+# do not clone the entire repo every time a file is added
+import filecmp
+import pathlib
+
+ALLOWED_EXTENSIONS = ['md']
+
+cmpr = filecmp.dircmp(pathlib.Path('./tests').resolve(), pathlib.Path('./tests').resolve())
+print(cmpr.common)
+
+# get path of ./tests
+tests = pathlib.Path('./tests').resolve()
